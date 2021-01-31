820da6447b4d4eb369d71a1b8bd1ee8f13e1ec27
Aleksey Sayenko <aleksey_sayenko@epam.com>
2021-01-31 04:05:01 +0200

    Add xcov

--- Dangerfile
+++
@@ -1,6 +0,0 @@
-xcov.report(
-  scheme: 'UpInterHomeWork21',
-  project: 'UpInterHomeWork21.xcodeproj',
-  xccov_file_direct_path: ENV['BITRISE_XCRESULT_PATH'],
-  include_targets: 'UpInterHomeWork21.app'
-)
--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,2 +1,13 @@
 import Danger 
+import DangerSwiftLint // package: https://github.com/ashfurrow/danger-swiftlint.git
+
 let danger = Danger()
+
+SwiftLint.lint()
+
+xcov.report(
+  scheme: 'UpInterHomeWork21',
+  project: 'UpInterHomeWork21.xcodeproj',
+  xccov_file_direct_path: ENV['BITRISE_XCRESULT_PATH'],
+  include_targets: 'UpInterHomeWork21.app'
+)
